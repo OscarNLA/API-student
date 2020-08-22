c1c686d7ba19a0a7212b55aed256ba66116327d6
OscarNLA <a2019114151@estudiantes.upsa.edu.bo>
2020-08-22 12:02:15 -0400

    se agrego servidor

---
+++ APIstudent/APIstudent/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace APIstudent.Models
+{
+    public class DataContext:DbContext 
+    {
+        public DataContext(): base("Default Connection")
+        {
+
+
+        }
+    }
+}
